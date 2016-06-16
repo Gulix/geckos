82543a2d98ef3a65726bd8766e486642d79ccc1f
Nicolas Ronvel <gulix33xp@yahoo.fr>
2016-06-16 13:27:59 +0200

    Following the Issue #12 - Rich Text Field
    Using a Knockout Custom Binding to make it easier

---
+++ src/js/knockoutExtensions.js
@@ -0,0 +1,54 @@
+/*******************************************************************************/
+/* wysihtml5 allows to bind a textarea to a text value, using wysihtml5 editor */
+/* Based on https://github.com/nicholasjackson/knockoutjs-wysihtml5            */
+/*******************************************************************************/
+ko.bindingHandlers.wysihtml5 = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+
+        var options = {};
+        var value = ko.utils.unwrapObservable(valueAccessor()) || {};
+
+       if (value.options) {
+            ko.utils.extend(options, value.options);
+            delete value.options;
+        }
+
+        // if the textarea has no id, generate one to keep wysihtml5 happy
+        if($(element).attr('id') == undefined || $(element).attr('id') == '')
+            $(element).attr('id','id_' + Math.floor(new Date().valueOf()));
+        var idValue = $(element).attr('id');
+        var editor = new wysihtml5.Editor(idValue, options);
+        $(element).data("wysihtml5", editor);
+
+
+        editor.on('change', function() {
+          var observable;
+          var content = ko.utils.unwrapObservable(valueAccessor()) || {};
+
+          if (content.data != undefined) {
+              observable = valueAccessor().data;
+          } else {
+              observable = valueAccessor();
+          }
+          var control = $(element).data("wysihtml5");
+          if (control != undefined) {
+            observable(control.getValue());
+          }
+        });
+
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        //
+        var control = $(element).data("wysihtml5");
+        if (control != undefined) {
+          var content = ko.utils.unwrapObservable(valueAccessor()) || {};
+
+          if (content.data != undefined) {
+              control.setValue(valueAccessor().data(), false);
+          } else {
+              control.setValue(valueAccessor()(), false);
+          }
+        }
+
+    }
+};
