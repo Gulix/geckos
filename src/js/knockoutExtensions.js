e12b9988d2fc2ac3ee85ca7f7e45b373512bfb01
Nicolas Ronvel <gulix33xp@yahoo.fr>
2016-06-19 15:07:42 +0200

    All knockout extensions in one file

--- src/js/knockoutExtensions.js
+++ src/js/knockoutExtensions.js
@@ -52,3 +52,14 @@ ko.bindingHandlers.wysihtml5 = {
 
     }
 };
+
+/* Allows to bind a canvas for FabricJS, with the Json Generated template value */
+ko.bindingHandlers.fabric = {
+
+    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var viewModel = bindingContext.$data;
+        var jsonValue = viewModel.generatedTemplate();
+        var canvas = viewModel.canvas;
+        canvas.loadFromJSON(jsonValue, canvas.renderAll.bind(canvas));
+    }
+};
