d1c0a1d41bada8086747f3e3781b6d77e61e33b4
Nicolas Ronvel <gulix33xp@yahoo.fr>
2016-08-16 10:53:34 +0200

    Renaming variables for better comprehension

--- src/js/inheriting-styles.js
+++ src/js/inheriting-styles.js
@@ -1,79 +1,67 @@
 define(['utils'], function(Utils) {
 
   // Create a Style object, based on another style
-  function getStyleFromBase(heritingStyle, heritedStyle) {
+  function getStyleFromBase(parentStyle, childStyle) {
     var builtStyle = { };
 
     // Basic properties
-    builtStyle.name = (heritingStyle.name == undefined) ? heritedStyle.name : heritingStyle.name;
-    builtStyle.description = (heritingStyle.description == undefined) ? heritedStyle.description : heritingStyle.description;
-    builtStyle.canvasBackground = (heritingStyle.canvasBackground == undefined) ? heritedStyle.canvasBackground : heritingStyle.canvasBackground;
-    builtStyle.canvasWidth = (heritingStyle.canvasWidth == undefined) ? heritedStyle.canvasWidth : heritingStyle.canvasWidth;
-    builtStyle.canvasHeight = (heritingStyle.canvasHeight == undefined) ? heritedStyle.canvasHeight : heritingStyle.canvasHeight;
+    builtStyle.name = (parentStyle.name == undefined) ? childStyle.name : parentStyle.name;
+    builtStyle.description = (parentStyle.description == undefined) ? childStyle.description : parentStyle.description;
+    builtStyle.canvasBackground = (parentStyle.canvasBackground == undefined) ? childStyle.canvasBackground : parentStyle.canvasBackground;
+    builtStyle.canvasWidth = (parentStyle.canvasWidth == undefined) ? childStyle.canvasWidth : parentStyle.canvasWidth;
+    builtStyle.canvasHeight = (parentStyle.canvasHeight == undefined) ? childStyle.canvasHeight : parentStyle.canvasHeight;
 
     // Fields, identified by 'name'
     builtStyle.fields = [ ]; // TODO : need to think about an 'order of fields' with this inheritance
-    // Adding all the fields existing in the heritedStyle (modified by the heritingStyle, if needed)
-    for(var iHeritedField = 0; iHeritedField < heritedStyle.fields.length; iHeritedField++) {
-      var heritedField = Utils.clone(heritedStyle.fields[iHeritedField]);
-      var heritingField = getFieldByName(heritingStyle.fields, heritedField.name);
-      if (heritingField == null) {
-        // Field not modified by heritingStyle
-        builtStyle.fields.push(heritedField);
-      } else {
-        //builtStyle.fields.push(getFieldFromInheritance(heritingField, heritedField));
-        builtStyle.fields.push(Object.assign(heritedField, heritingField));
+    // Adding all the fields existing in the childStyle (modified by the parentStyle, if needed)
+    for(var iChildField = 0; iChildField < childStyle.fields.length; iChildField++) {
+      var childField = Utils.clone(childStyle.fields[iChildField]);
+      var parentField = getFieldByName(parentStyle.fields, childField.name);
+      if (parentField == null) {
+        // Field not modified by parentStyle
+        builtStyle.fields.push(childField);
+      } else if (heri) {
+        builtStyle.fields.push(Object.assign(childField, parentField));
       }
     }
-    // Adding the fields existing only in the heritingStyle
-    for (var iHeritingField = 0; iHeritingField < heritingStyle.fields.length; iHeritingField++) {
-      var heritingField = Utils.clone(heritingStyle.fields[iHeritingField]);
-      var heritedField = getFieldByName(heritedStyle.fields, heritingField.name);
-      if (heritedField == null) {
-        builtStyle.fields.push(heritingField);
+    // Adding the fields existing only in the parentStyle
+    for (var iParentField = 0; iParentField < parentStyle.fields.length; iParentField++) {
+      var parentField = Utils.clone(parentStyle.fields[iParentField]);
+      var childField = getFieldByName(childStyle.fields, parentField.name);
+      if (childField == null) {
+        builtStyle.fields.push(parentField);
       }
     }
 
     // Canvas fields, identified by 'id' (if no id, fields always added)
     builtStyle.canvasFields = [ ]; // TODO : need to think about an 'order of fields / z-index' with this inheritance
-    // Adding all the canvasFields existing in the heritedStyle (modified by the heritingStyle, if needed)
-    for(var iHeritedField = 0; iHeritedField < heritedStyle.canvasFields.length; iHeritedField++) {
-      var heritedField = Utils.clone(heritedStyle.canvasFields[iHeritedField]);
-      var heritingField = null;
-      if (heritedField.id != undefined) {
-        heritingField = getCanvasFieldById(heritingStyle.canvasFields, heritedField.id);
+    // Adding all the canvasFields existing in the childStyle (modified by the parentStyle, if needed)
+    for(var iChildField = 0; iChildField < childStyle.canvasFields.length; iChildField++) {
+      var childField = Utils.clone(childStyle.canvasFields[iChildField]);
+      var parentField = null;
+      if (childField.id != undefined) {
+        parentField = getCanvasFieldById(parentStyle.canvasFields, childField.id);
       }
-      if (heritingField == null) {
-        // Field not modified by heritingStyle
-        builtStyle.canvasFields.push(heritedField);
+      if (parentField == null) {
+        // Field not modified by parentStyle
+        builtStyle.canvasFields.push(childField);
       } else {
-        //builtStyle.canvasFields.push(getFieldFromInheritance(heritingField, heritedField));
-        builtStyle.canvasFields.push(Object.assign(heritedField, heritingField));
+        builtStyle.canvasFields.push(Object.assign(childField, parentField));
       }
     }
-    // Adding the canvasFields existing only in the heritingStyle
-    for (var iHeritingField = 0; iHeritingField < heritingStyle.canvasFields.length; iHeritingField++) {
-      var heritingField = Utils.clone(heritingStyle.canvasFields[iHeritingField]);
-      var heritedField = null;
-      if (heritingField.id != undefined) {
-        heritedField = getCanvasFieldById(heritedStyle.canvasFields, heritingField.id);
+    // Adding the canvasFields existing only in the parentStyle
+    for (var iParentField = 0; iParentField < parentStyle.canvasFields.length; iParentField++) {
+      var parentField = Utils.clone(parentStyle.canvasFields[iParentField]);
+      var childField = null;
+      if (parentField.id != undefined) {
+        childField = getCanvasFieldById(childStyle.canvasFields, parentField.id);
       }
-      if (heritedField == null) {
-        builtStyle.canvasFields.push(heritingField);
+      if (childField == null) {
+        builtStyle.canvasFields.push(parentField);
       }
     }
 
     return builtStyle;
-  }
-
-  function getFieldFromInheritance(heritingField, heritedField) {
-    var field = heritedField;
-    for(var key in field) {
-      if (heritingField[key] != null) {
-        field[key] = heritingField[key];
-      }
-    }
-    return field;
   }
 
   function getFieldByName(fields, name) {
@@ -97,6 +85,6 @@ define(['utils'], function(Utils) {
   }
 
   return {
-    getStyleFromBase: function(heritingStyle, heritedStyle) { return getStyleFromBase(heritingStyle, heritedStyle); }
+    getStyleFromBase: function(parentStyle, childStyle) { return getStyleFromBase(parentStyle, childStyle); }
   }
 });
