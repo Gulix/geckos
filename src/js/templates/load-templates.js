f510b81918bab9752fa2698807512f3531c39d82
Nicolas Ronvel <gulix33xp@yahoo.fr>
2016-12-21 19:16:36 +0100

    Updated load-templates file

--- src/js/templates/load-templates.js
+++ src/js/templates/load-templates.js
@@ -5,9 +5,10 @@ define([
 'json!templates/bloodbowl-starplayerv3.json', 
 'json!templates/pulpalley-charactercard.json', 
 'json!templates/pulpalley-fortunecard.json', 
-'json!templates/pulpcity-agendas.json'], 
+'json!templates/pulpcity-agendas.json', 
+'json!templates/wquest-silvertower-herocard.json'], 
  function(
-tpl1, tpl2, tpl3, tpl4, tpl5, tpl6, tpl7) 
+tpl1, tpl2, tpl3, tpl4, tpl5, tpl6, tpl7, tpl8) 
  { return { load: function() {
 var list = [ ];
 list.push(tpl1);
@@ -17,6 +18,7 @@ list.push(tpl4);
 list.push(tpl5);
 list.push(tpl6);
 list.push(tpl7);
+list.push(tpl8);
 return list;
 
 } }; });
