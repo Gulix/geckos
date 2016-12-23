dbf1a777b4858cb0ab73eb5244c7cc1b7d673053
Nicolas Ronvel <gulix33xp@yahoo.fr>
2016-12-23 15:47:53 +0100

    Added "Au nom de l'Empereur" template

--- js/templates/load-templates.js
+++ js/templates/load-templates.js
@@ -1,5 +1,6 @@
 define([
 'json!templates/arabesque-event.json', 
+'json!templates/AuNomEmpereurCharacter.json', 
 'json!templates/basic-effects.json', 
 'json!templates/bloodbowl-starplayers.json', 
 'json!templates/bloodbowl-starplayerv3.json', 
@@ -8,7 +9,7 @@ define([
 'json!templates/pulpcity-agendas.json', 
 'json!templates/wquest-silvertower-herocard.json'], 
  function(
-tpl1, tpl2, tpl3, tpl4, tpl5, tpl6, tpl7, tpl8) 
+tpl1, tpl2, tpl3, tpl4, tpl5, tpl6, tpl7, tpl8, tpl9) 
  { return { load: function() {
 var list = [ ];
 list.push(tpl1);
@@ -19,6 +20,7 @@ list.push(tpl5);
 list.push(tpl6);
 list.push(tpl7);
 list.push(tpl8);
+list.push(tpl9);
 return list;
 
 } }; });
