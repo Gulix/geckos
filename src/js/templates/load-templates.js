08ceb7cb3dce341393fb4e30e8484f99b44ab401
Nicolas Ronvel <gulix33xp@yahoo.fr>
2017-01-30 21:54:39 +0100

    REmoving 'In Development' templates

--- src/js/templates/load-templates.js
+++ src/js/templates/load-templates.js
@@ -1,15 +1,12 @@
 define([
 'json!templates/arabesque-event.json', 
 'json!templates/AuNomEmpereurCharacter.json', 
-'json!templates/basic-effects.json', 
-'json!templates/bloodbowl-starplayers.json', 
 'json!templates/bloodbowl-starplayerv3.json', 
 'json!templates/pulpalley-charactercard.json', 
 'json!templates/pulpalley-fortunecard.json', 
-'json!templates/pulpcity-agendas.json', 
 'json!templates/wquest-silvertower-herocard.json'], 
  function(
-tpl1, tpl2, tpl3, tpl4, tpl5, tpl6, tpl7, tpl8, tpl9) 
+tpl1, tpl2, tpl3, tpl4, tpl5, tpl6) 
  { return { load: function() {
 var list = [ ];
 list.push(tpl1);
@@ -18,9 +15,6 @@ list.push(tpl3);
 list.push(tpl4);
 list.push(tpl5);
 list.push(tpl6);
-list.push(tpl7);
-list.push(tpl8);
-list.push(tpl9);
 return list;
 
 } }; });
