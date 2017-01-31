b83f3c30c974653f203c48894528b39d86453c08
Nicolas Ronvel <gulix33xp@yahoo.fr>
2017-01-31 12:46:57 +0100

    Reintegration of Basic Effects Templates
    Fix #146  through the completion of #152

--- src/js/templates/load-templates.js
+++ src/js/templates/load-templates.js
@@ -1,12 +1,13 @@
 define([
 'json!templates/arabesque-event.json', 
 'json!templates/AuNomEmpereurCharacter.json', 
+'json!templates/basic-effects.json', 
 'json!templates/bloodbowl-starplayerv3.json', 
 'json!templates/pulpalley-charactercard.json', 
 'json!templates/pulpalley-fortunecard.json', 
 'json!templates/wquest-silvertower-herocard.json'], 
  function(
-tpl1, tpl2, tpl3, tpl4, tpl5, tpl6) 
+tpl1, tpl2, tpl3, tpl4, tpl5, tpl6, tpl7) 
  { return { load: function() {
 var list = [ ];
 list.push(tpl1);
@@ -15,6 +16,7 @@ list.push(tpl3);
 list.push(tpl4);
 list.push(tpl5);
 list.push(tpl6);
+list.push(tpl7);
 return list;
 
 } }; });
