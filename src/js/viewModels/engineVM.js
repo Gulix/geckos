0f1cd9beddb0826592d8a55e0a58846616732ebf
Nicolas Ronvel <gulix33xp@yahoo.fr>
2017-03-31 20:01:37 +0200

    Issue #158  & #157  - Refining the export code
    Also added the export of all cards as a ZIP containing SVG

--- src/js/viewModels/engineVM.js
+++ src/js/viewModels/engineVM.js
@@ -118,58 +118,76 @@ define(['knockout',
 
     /* Export the content of the Canvas as a PNG */
     /* TODO : Getting a dedicated object / function to export a card (template + data) as a PNG */
+
+    // Get the currend card as a PNG file to save on the client computer
+    self.exportAsPNG = function() {
+      self.exportCurrentCardAsPNG(self.saveBlobOnComputer);
+    }
+    // Get the currend card as a SVG file to save on the client computer
+    self.exportAsSVG = function() {
+      self.exportCurrentCardAsSVG(self.saveBlobOnComputer);
+    }
+    // Get all the cards as a ZIP file of PNG files
     self.exportAllCardsToPngZip = function() {
       var zip = new jszip();
-      self.cardsToPng(0, self.addToZip, zip);
+      self.exportSingleCardToZip(0, self.exportCurrentCardAsPNG, zip);
     }
-
+    // Get all the cards as a ZIP file of SVG files
     self.exportAllCardsToSvgZip = function() {
       var zip = new jszip();
-      self.cardsToSvg(0, self.addToZip, zip);
+      self.exportSingleCardToZip(0, self.exportCurrentCardAsSVG, zip);
     }
 
-    self.exportPng = function() {
+    // Current selected card is exported in PNG
+    // Following action with the generated blob depends on parameter
+    self.exportCurrentCardAsPNG = function(actionOnBlob) {
       if (self.editableCard() != null) {
         var canvas = document.getElementById('fabricjs-canvas');
         canvas.toBlob(function(blob) {
-          saveAs(blob, self.editableCard().cardName() + ".png");
+          actionOnBlob(blob, self.editableCard().cardName() + ".png");
         });
       }
     }
 
-    self.exportSvg = function() {
+    // Current selected card is exported in PNG
+    // Following action with the generated blob depends on parameter
+    self.exportCurrentCardAsSVG = function(actionOnBlob) {
       if (self.editableCard() != null) {
         if (self.canvas != null) {
           var svg = self.canvas.toSVG();
           var blob = new Blob([svg], {type: "image/svg+xml"});
-          saveAs(blob, self.editableCard().cardName() + ".svg");
+          actionOnBlob(blob, self.editableCard().cardName() + ".svg");
         }
       }
     }
 
+    // Save a blob (SVG or PNG) as a file on the computer
+    self.saveBlobOnComputer = function(blob, fullCardName) {
+      saveAs(blob, fullCardName);
+    }
 
-    self.cardsToPng = function(iCardIndex, blobAction, zipper) {
+
+    self.exportSingleCardToZip = function(iCardIndex, exportBlob, zipper) {
       if ((iCardIndex >= 0) && (iCardIndex < self.listCards().length)) {
         self.editableCard(self.listCards()[iCardIndex]);
+
+        // Timeout is needed in order to ensure the canvas is fully loaded
         setTimeout(function() {
-          var canvas = document.getElementById('fabricjs-canvas');
-          canvas.toBlob(function(blob) {
-            blobAction(blob, iCardIndex, zipper);
-          });
-        }, 500); // Need to find a better way to launch action when the canvas is ready
+          exportBlob(function(blob, fullCardName) { self.addBlobFileToZip(blob, iCardIndex, zipper, fullCardName, exportBlob); });
+        }, 500); // TODO - Need to find a better way to launch action when the canvas is ready
       } else if (iCardIndex >= self.listCards().length) {
         zipper.generateAsync({type:"blob"})
           .then(function(content) {
-            saveAs(content, "example.zip");
+            saveAs(content, "allMyCards.zip");
         });
       }
     }
 
-    self.addToZip = function(blob, iCardIndex, zipper, extension) {
+    self.addBlobFileToZip = function(blob, iCardIndex, zipper, fullCardName, actionOnBlob) {
       // TODO - Check for 'same name' cards
 
-      zipper.file(self.editableCard().cardName() + "." + extension, blob);
-      self.cardsToPng(iCardIndex + 1, self.addToZip, zipper);
+      zipper.file(fullCardName, blob);
+      self.exportSingleCardToZip(iCardIndex + 1, actionOnBlob, zipper);
     };
 
     /* ------------------------------- */
