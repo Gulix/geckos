50e87ea343f18980f7ed08b0562a58289c52ba59
Nicolas Ronvel <gulix33xp@yahoo.fr>
2017-05-15 13:32:54 +0200

    Update DW templates
    Minor improvements on the dw-lieu template
    Added back all the templates in the loading file

--- src/js/templates/load-templates.js
+++ src/js/templates/load-templates.js
@@ -1,10 +1,30 @@
 define([
-'json!templates/dw-lieu.json'],
+'json!templates/apres-accident.json', 
+'json!templates/arabesque-event.json', 
+'json!templates/AuNomEmpereurCharacter.json', 
+'json!templates/basic-effects.json', 
+'json!templates/bloodbowl-starplayerv3.json', 
+'json!templates/dungeonworld-moves.json', 
+'json!templates/dw-lieu.json', 
+'json!templates/pulpalley-charactercard.json', 
+'json!templates/pulpalley-fortunecard.json', 
+'json!templates/pulpcity-misc.json', 
+'json!templates/wquest-silvertower-herocard.json'], 
  function(
-tpl1)
+tpl1, tpl2, tpl3, tpl4, tpl5, tpl6, tpl7, tpl8, tpl9, tpl10, tpl11) 
  { return { load: function() {
 var list = [ ];
 list.push(tpl1);
+list.push(tpl2);
+list.push(tpl3);
+list.push(tpl4);
+list.push(tpl5);
+list.push(tpl6);
+list.push(tpl7);
+list.push(tpl8);
+list.push(tpl9);
+list.push(tpl10);
+list.push(tpl11);
 return list;
 
 } }; });
