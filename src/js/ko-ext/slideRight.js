6629f56961615150aed6dfccc4585ed2b5c2b308
Nicolas Ronvel <gulix33xp@yahoo.fr>
2017-06-09 10:54:18 +0200

    Correcting the slide on the menu

--- src/js/ko-ext/slideRight.js
+++ src/js/ko-ext/slideRight.js
@@ -3,15 +3,13 @@ define(['knockout', 'jQuery'], function(ko) {
 ko.bindingHandlers.slideRightMenu = {
     init: function(element, valueAccessor) {
         // Initially set the element to be instantly visible/hidden depending on the value
-        $(element).closest("li").hover(
-          function() { $(element).show(400); },
-          function() { $(element).hide(400); }
-        );
-        $(element).hide(); // Use "unwrapObservable" so we can handle values that may or may not be observable
+        var value = valueAccessor();
+        $(element).toggle(ko.unwrap(value));
     },
     update: function(element, valueAccessor) {
         // Whenever the value subsequently changes, slowly fade the element in or out
-
+        var value = valueAccessor();
+        ko.unwrap(value) ? $(element).show(400) : $(element).hide(400);
     }
 };
 
