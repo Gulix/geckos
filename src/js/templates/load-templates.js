6c2d45a0d3257c31635ec80a53b6383ac9ff55b9
Nicolas Ronvel <gulix33xp@yahoo.fr>
2017-07-20 12:35:31 +0200

    Preparing for new release

--- src/js/templates/load-templates.js
+++ src/js/templates/load-templates.js
@@ -1,17 +1,16 @@
 define([
-'json!templates/apres-accident.json', 
-'json!templates/arabesque-event.json', 
-'json!templates/AuNomEmpereurCharacter.json', 
-'json!templates/basic-effects.json', 
-'json!templates/bloodbowl-starplayerv3.json', 
-'json!templates/dungeonworld-moves.json', 
-'json!templates/dw-lieu.json', 
-'json!templates/pulpalley-charactercard.json', 
-'json!templates/pulpalley-fortunecard.json', 
-'json!templates/pulpcity-misc.json', 
-'json!templates/wquest-silvertower-herocard.json'], 
+'json!templates/apres-accident.json',
+'json!templates/arabesque-event.json',
+'json!templates/AuNomEmpereurCharacter.json',
+'json!templates/basic-effects.json',
+'json!templates/bloodbowl-starplayerv3.json',
+'json!templates/dungeonworld-moves.json',
+'json!templates/pulpalley-charactercard.json',
+'json!templates/pulpalley-fortunecard.json',
+'json!templates/pulpcity-misc.json',
+'json!templates/wquest-silvertower-herocard.json'],
  function(
-tpl1, tpl2, tpl3, tpl4, tpl5, tpl6, tpl7, tpl8, tpl9, tpl10, tpl11) 
+tpl1, tpl2, tpl3, tpl4, tpl5, tpl6, tpl7, tpl8, tpl9, tpl10)
  { return { load: function() {
 var list = [ ];
 list.push(tpl1);
@@ -24,7 +23,6 @@ list.push(tpl7);
 list.push(tpl8);
 list.push(tpl9);
 list.push(tpl10);
-list.push(tpl11);
 return list;
 
 } }; });
